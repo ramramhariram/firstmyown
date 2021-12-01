638b5cc4fcaaa7f0ffc6eabd4c7adb35c24da2da
Hariram Sankaran <56744845+ramramhariram@users.noreply.github.com>
2021-12-01 12:45:01 -0800

    cts passing only services changes

--- main.tf
+++ main.tf
@@ -8,11 +8,16 @@ terraform {
 }
 
 resource "aws_elb_attachment" "elb_underlay" {
-	for_each = var.services
+  for_each = local.cts_services
   elb      = var.elbname
   instance = each.value.node
 }
 
 provider "aws" {
   region = var.region
+}
+
+
+locals {
+  cts_services = { for k,v in var.services : k => v if v["status"] == "passing" }
 }
